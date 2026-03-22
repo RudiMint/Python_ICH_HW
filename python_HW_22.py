0ef1dcb0858a0abf7aca2102f3c17c6a5c8ebe64
RudiMint <ashevchenko629@gmail.com>
2026-03-22 16:27:12 +0100

    homework with lambda

---
+++ python_HW_22.py
@@ -0,0 +1,41 @@
+# task 1
+
+orders = [
+    {"product": "Laptop", "price": 1200},
+    {"product": "Mouse", "price": 50},
+    {"product": "Keyboard", "price": 100},
+    {"product": "Monitor", "price": 300},
+    {"product": "Chair", "price": 800},
+    {"product": "Desk", "price": 400}
+]
+def sorted_orders(data):
+    result = []
+    for d in data:
+        if d["price"] > 500:
+            result.append(d["product"])
+    return result
+
+products_list = sorted(sorted_orders(orders), key=lambda x: x[0].lower())
+
+print(products_list)
+
+
+# task 2
+
+sales = [
+    ("Laptop", 5, 1200),
+    ("Mouse", 50, 20),
+    ("Keyboard", 30, 50),
+    ("Monitor", 10, 300),
+    ("Chair", 20, 800)
+]
+
+# price = lambda x, y: x * y
+# sales_dict = {k[0]: price(k[1], k[2]) for k in sales}
+# sorted_sales = dict(sorted(sales_dict.items(), key=lambda x: x[1], reverse=True))
+
+
+sorted_sales = dict(sorted(
+    {product: amount * price for product, amount, price in sales}.items(), key=lambda x: x[1], reverse=True)
+)
+print(sorted_sales)
