c3ec4e8629ac0415e72508bac8670376858faff0
RudiMint <ashevchenko629@gmail.com>
2026-05-04 10:57:00 +0200

    exceptions + logging

---
+++ python_HW_25.py
@@ -0,0 +1,39 @@
+import logging
+
+logging.basicConfig(
+    filename="error.log",
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
+    level=logging.ERROR
+)
+
+def divide():
+    """
+        Prompts the user to enter two numbers (dividend and divisor),
+        performs division, and handles possible errors.
+
+        Handled exceptions:
+        - ValueError: if the input is not a valid number
+        - ZeroDivisionError: if attempting to divide by zero
+
+        The function keeps asking for input until valid data is provided.
+        After a successful division, it prints the result.
+    """
+    while True:
+        try:
+            first = int(input("enter first number >>> "))
+            second = int(input("enter second number >>> "))
+            result = first / second
+            return result
+        except (ValueError, ZeroDivisionError) as e:
+            print(f"exception: {e}")
+            logging.error(f"exception: {e}")
+
+print(divide())
+
+# Логирование ошибок
+#
+# Перенаправьте в предыдущей задаче вывод ошибок в файл errors.log в соответствии с форматом ниже.
+#
+# Пример вывода:
+#
+# 2025-02-23 22:38:53,686 - ERROR - test.py - 16 - Ошибка: Введено некорректное число.
