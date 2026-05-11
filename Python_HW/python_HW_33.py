7ed9adcb47a708a083487d3c83597b7eba411fff
RudiMint <ashevchenko629@gmail.com>
2026-05-11 20:26:52 +0200

    decorator with arguments. docstrings added

--- Python_HW/python_HW_33.py
+++ Python_HW/python_HW_33.py
@@ -4,9 +4,32 @@ from functools import wraps
 
 
 def measure_time(repeats):
+    """
+    Decorator factory for measuring average execution time.
+    Args:
+        repeats (int): Number of times the function should be executed.
+    Returns:
+        function: Decorated function with timing measurement.
+    """
     def decorator(func):
+        """
+        Decorates a function and measures its average runtime.
+        Args:
+            func (function): Function to decorate.
+        Returns:
+            function: Wrapped function.
+        """
         @wraps(func)
         def wrapper(*args, **kwargs):
+            """
+            Executes the function multiple times and
+            prints the average execution time.
+            Args:
+                *args: Positional arguments for the function.
+                **kwargs: Keyword arguments for the function.
+            Returns:
+                Any: Result of the last function execution.
+            """
             times = []
 
             for i in range(repeats):
@@ -26,6 +49,11 @@ def measure_time(repeats):
 
 @measure_time(10)
 def compute():
+    """
+    Calculates the sum of numbers from 0 to 9,999,999.
+    Returns:
+        int: Computed sum.
+    """
     total = 0
 
     for i in range(10_000_000):
